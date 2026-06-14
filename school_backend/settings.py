8df6d4771eaad7e838ceee4bbc229bcbb1ef6a6a
za3ma3 <meriche.isl4m@gmail.com>
2026-06-14 17:39:34 +0100

    removed google sid form backend

--- school_backend/settings.py
+++ school_backend/settings.py
@@ -34,7 +34,7 @@ else:
     CHARGILY_API_KEY = config('CHARGILY_PRIVATE')
     CHARGILY_BASE_URL = 'https://pay.chargily.net/api/v2'
 FRONTEND_URL = config('FRONTEND_URL', default='http://localhost:5173')
-GOOGLE_OAUTH_CLIENT_ID = config('GOOGLE_OAUTH_CLIENT_ID')
+
 TWILIO_USE_TEST = True
 if TWILIO_USE_TEST:
     TWILIO_ACCOUNT_SID = config('TEST_TWILIO_ACCOUNT_SID')
